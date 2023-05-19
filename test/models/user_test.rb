d18bc78f6cd4e83f26080ca8156486b3a85ff213
= <=>
2023-05-19 16:59:48 +0100

    user model & tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'user with valid email should be valid' do
+    user = User.new(email: 'mhz@test.com', password_digest: 'test')
+    assert user.valid?
+  end
+
+  test 'user with invalid email should not be valid' do
+    user = User.new(email: 'mhztest.com', password_digest:'test')
+
+    assert_not user.valid?
+  end
+
+  test 'user with taken email should not be valid' do
+    old_user = users(:one)
+    new_user = User.new(email: old_user.email, password_digest: 'test')
+    assert_not new_user.valid?
+  end
 end
